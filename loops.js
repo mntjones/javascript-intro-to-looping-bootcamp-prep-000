1ba209ce921439f725671889a45014bb4fb72dbb
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-21 18:29:30 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop (arr) {
+  for (var i=0; i <25; i++) {
+    if (i === 0) {
+      console.log("I am 1 strange loop.")
+    }
+    
+    else {
+      console.log("I am ${i} strange loops." )
+    }
+  }
+}

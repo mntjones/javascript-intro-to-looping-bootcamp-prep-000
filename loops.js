a9d89ccc31a2976f6622bfa0d359f6ab07de33b8
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-21 18:40:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,14 @@
 function forLoop (arr) {
   for (let i=0; i <25; i++) {
     if (i === 1) {
-      console.log("I am 1 strange loop.")
+      arr.push("I am 1 strange loop.")
     }
     
     else {
-      console.log("I am  " + i + " strange loops." )
+      arr.push("I am  " + i + " strange loops." )
     }
   }
+  return arr
 }
 
 function whileLoop(num) {
